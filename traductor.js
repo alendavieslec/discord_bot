82c20ceae4cb31d83f96f58de6c976f732eca7af
Luca Lazcano <lazcanoluca@gmail.com>
2022-04-22 21:33:01 -0300

    dy

--- traductor.js
+++ traductor.js
@@ -1,9 +1,9 @@
 const Reverso = require('reverso-api');
 const reverso = new Reverso();
 
-exports.traducir = async function ( {texto = 'No se pasó texto', orig_len = '', target_len = 'English', callback = unaVezTraducido, reaction} = {} ) {
+exports.traducir = async function ( {texto = 'No se pasó texto', orig_len = 'Spanish', target_len = 'English', callback = unaVezTraducido, reaction, user} = {} ) {
     // const result =
-    await reverso.getTranslation(texto, orig_len, target_len).catch(err => console.error(err)).then(result => callback( result, reaction ));
+    await reverso.getTranslation(texto, orig_len, target_len).catch(err => console.error(err)).then(result => callback( {result: result, reaction: reaction, user: user} ));
     
     // callback(result);
 }
